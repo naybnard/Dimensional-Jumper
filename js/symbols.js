48017a843b467ee8d71bdffd8186d5e0f6f25f38
naybnard <naybnard@icloud.com>
2025-05-01 18:56:07 +0200

    Add symbols.js for game symbols.

---
+++ js/symbols.js
@@ -0,0 +1,8 @@
+const symbols = [
+    { name: "medieval-sword", src: "assets/images/medieval-sword-coin.png", value: 10 },
+    { name: "futuristic-robot", src: "assets/images/futuristic-robot-coin.png", value: 15 },
+    { name: "underwater-pearl", src: "assets/images/underwater-pearl-coin.png", value: 8 },
+    { name: "jungle-tiger", src: "assets/images/jungle-tiger-coin.png", value: 12 },
+    { name: "space-planet", src: "assets/images/space-planet-coin.png", value: 20 },
+    { name: "scatter", src: "assets/images/scatter-coin.png", value: 0 }
+];
